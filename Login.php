876319b02e43907e63587e8daf19973af5144506
riccairsa <110507641+riccairsa@users.noreply.github.com>
2022-11-14 13:13:21 +0700

    Add files via upload

---
+++ Login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+</head>
+<body>
+
+<p>Directory access is forbidden.</p>
+<li class="">
+	<a href="http://localhost/wpu-login/" target="_blank"></a>
+</li>
+
+</body>
+</html>
